ff48c6dc09da5cd861a615a2341f0dfa91dd633f
Ashish PANWAR <panwar.ashish10@gmail.com>
2021-05-12 20:58:26 +0530

    variable description added

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # aws-terraform-backend-module
 AWS Terraform backend module with S3 and DynamoDB
+
+### Pull backend-module
+```
+ module "backend-module" {
+  source  = "cloudxperts/backend-module/aws"
+  version = "1.0.0"
+  # insert the 1 required variable here
+}
+```
--- variable.tf
+++ variable.tf
@@ -1,15 +1,17 @@
 variable "aws-region" {
+   description = "Provide the region of your choice, default us-east-1."
    type = string
    default = "us-east-1"
 }
 
 variable "aws-profile" {
+   description = "Please provide the IAM profile which will connect to AWS. default is terraform which will work only if terraform profile is created."
    type = string
    default = "terraform"
 }
 
 variable "bucket_name" {
-   description = "bucket name must contain string number and - special char, no other char"
+   description = "Bucket name must be unique globally and should not contain any special char except '-' char."
    type = string
 
    validation {
@@ -19,16 +21,18 @@ variable "bucket_name" {
 }
 
 variable "versioning_enabled" {
+   description = "This variable's value must be true or false."
    type = bool
    default = true
 }
 
 variable "dynamodb_table" {
+   description = "Provide your dynamoDB table name, default cx-terraform-backend-lock-table"
    type = string
    default = "cx-terraform-backend-lock-table"
    validation {
-      condition = length(var.dynamodb_table) < 60
-      error_message = "Table name must be less than 60 char."
+      condition = length(var.dynamodb_table) < 60 && length(var.dynamodb_table) > 5
+      error_message = "Table name should be less than 60 char and greater than 5 char."
    }
 }
 
